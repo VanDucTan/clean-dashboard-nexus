86326e21b3cd6359c20434a0a10ce1663fd756e8
YourName <your.email@example.com>
2025-04-15 10:04:55 +0700

    create ui for recruiment part

--- src/components/ui/use-toast.tsx
+++ src/components/ui/use-toast.tsx
@@ -1,9 +1,5 @@
 import * as React from "react"
-
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toast"
+import { type ToastActionElement, type ToastProps } from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
@@ -90,8 +86,6 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId)
       } else {
@@ -112,6 +106,7 @@ export const reducer = (state: State, action: Action): State => {
         ),
       }
     }
+
     case "REMOVE_TOAST":
       if (action.toastId === undefined) {
         return {
@@ -188,4 +183,4 @@ function useToast() {
   }
 }
 
-export { useToast, toast }
+export { useToast, toast } 
